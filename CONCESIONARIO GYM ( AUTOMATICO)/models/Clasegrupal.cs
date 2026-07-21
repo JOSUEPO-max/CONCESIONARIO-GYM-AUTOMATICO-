0b10a7af2deccb8bbff0f4ffb3a9bcd23744b24d
Aron <javieraronverasantacruz@gmail.com>
2026-07-20 23:05:53 -0500

    implementar clases ClaseGrupal y Entrenador con manejo de cupos
    - Se crea la clase ClaseGrupal con atributos encapsulados y validacion de limite de cupos.
    - Se implementa el metodo ReservarCupo() para decrementar la disponibilidad de lugares.
    - Se construye la clase Entrenador con validacion de nombres no vacios y metodos de visualizacion."

---
+++ CONCESIONARIO GYM ( AUTOMATICO)/models/Clasegrupal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CONCESIONARIO_GYM___AUTOMATICO_.models
+{
+public class ClaseGrupal
+{
+    private int id;
+    private string nombreDisciplina;
+    private string horario;
+    private int cuposDisponibles;
+
+    public int Id { get => id; set => id = value; }
+    public string NombreDisciplina
+    {
+        get => nombreDisciplina;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new Exception("El nombre de la disciplina no puede estar vacío.");
+            }
+            nombreDisciplina = value;
+        }
+    }
+    public string Horario { get => horario; set => horario = value; }
+    public int CuposDisponibles
+    {
+        get => cuposDisponibles;
+        set
+        {
+            if (value < 0 || value > 50)
+            {
+                throw new Exception("Los cupos disponibles deben estar entre 0 y 50.");
+            }
+            cuposDisponibles = value;
+        }
+    }
+
+    public ClaseGrupal(int id, string nombreDisciplina, string horario, int cuposDisponibles)
+    {
+        this.Id = id;
+        this.NombreDisciplina = nombreDisciplina;
+        this.Horario = horario;
+        this.CuposDisponibles = cuposDisponibles;
+    }
+
+    public bool ReservarCupo()
+    {
+        if (this.CuposDisponibles > 0)
+        {
+            this.CuposDisponibles--;
+            Console.WriteLine($"Cupo reservado con éxito en {this.NombreDisciplina}.");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"No hay cupos disponibles para {this.NombreDisciplina}.");
+            return false;
+        }
+    }
+
+    public void MostrarDetalle()
+    {
+        Console.WriteLine($"[ID: {this.Id}] {this.NombreDisciplina} - Horario: {this.Horario} | Cupos: {this.CuposDisponibles}");
+    }
+}
+}
---
+++ CONCESIONARIO GYM ( AUTOMATICO)/models/Entrenador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CONCESIONARIO_GYM___AUTOMATICO_.models
+{
+    public class Entrenador
+    {
+        private int id;
+        private string nombre;
+        private string especialidad;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("El nombre del entrenador no puede estar vacío.");
+                }
+                nombre = value;
+            }
+        }
+        public string Especialidad { get => especialidad; set => especialidad = value; }
+
+        public Entrenador(int id, string nombre, string especialidad)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Especialidad = especialidad;
+        }
+
+        public void PresentarEntrenador()
+        {
+            Console.WriteLine($"[Entrenador #{this.Id}] {this.Nombre} - Especialidad: {this.Especialidad}");
+        }
+    }
+}
