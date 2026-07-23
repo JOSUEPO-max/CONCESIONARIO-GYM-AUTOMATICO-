7c5061531636dfbd8e18cf7b48f76c814eaa3e74
Josué Marcillo <edwin.marcillo@tecnologicoliceocristiano.edu.ec>
2026-07-22 22:09:58 -0500

    crear servicio GestionClases para reservas de actividades
    - Se crea la clase GestionClases con lista interna de objetos ClaseGrupal.
    - Se añade el metodo MostrarClasesDisponibles() para consultar horarios y cupos.
    - Se implementa AgendarCupoEnClase() buscando por identificador unico y descontando disponibilidad.

--- CONCESIONARIO GYM ( AUTOMATICO)/servicios/GestionClases.cs
+++ CONCESIONARIO GYM ( AUTOMATICO)/servicios/GestionClases.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using CONCESIONARIO_GYM___AUTOMATICO_.models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CONCESIONARIO_GYM___AUTOMATICO_.servicios
 {
-    internal class GestionClases
+    public class GestionClases
     {
+        private List<ClaseGrupal> listaClases;
+
+        public List<ClaseGrupal> ListaClases { get => listaClases; set => listaClases = value; }
+
+        public GestionClases()
+        {
+            this.ListaClases = new List<ClaseGrupal>();
+        }
+
+        public void AgregarClase(ClaseGrupal objClase)
+        {
+            if (objClase == null)
+            {
+                throw new Exception("No se puede agregar una clase nula.");
+            }
+            this.ListaClases.Add(objClase);
+            Console.WriteLine($"Clase {objClase.NombreDisciplina} registrada exitosamente.");
+        }
+
+        public void MostrarClasesDisponibles()
+        {
+            Console.WriteLine("--- CLASES GRUPALES DISPONIBLES ---");
+            foreach (ClaseGrupal objClase in this.ListaClases)
+            {
+                objClase.MostrarDetalle();
+            }
+        }
+
+        public void AgendarCupoEnClase(int idClase)
+        {
+            foreach (ClaseGrupal objClase in this.ListaClases)
+            {
+                if (objClase.Id == idClase)
+                {
+                    objClase.ReservarCupo();
+                    return;
+                }
+            }
+            Console.WriteLine("Error: El código de clase ingresado no existe.");
+        }
     }
 }
